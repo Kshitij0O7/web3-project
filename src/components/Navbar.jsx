d6840d1c21f22032c9fb85f1096fb4dd1ccf349d
dhanush <dhanushkamath@gmail.com>
2023-07-19 00:40:43 +0530

    Update Navbar.jsx
    added links to the nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import LOGO from '../assets/mainLogo.png'
+import { Link } from 'react-router-dom'
 function Navbar() {
 
   return (
@@ -10,9 +11,15 @@ function Navbar() {
           </div>
           <div className='mr-[5%] flex flex-column items-center space-x-5 pt-3'>
             <ul className='flex flex-column space-x-10'>
-              <li className=' text-2xl text-white font-normal' style={{fontFamily:'Dela Gothic One'}}>home</li>
-              <li className=' text-2xl text-white font-normal' style={{fontFamily:'Dela Gothic One'}}>Sell your item</li>
-              <li className=' text-2xl text-white font-normal' style={{fontFamily:'Dela Gothic One'}}>Auction</li>
+              <Link to={'/'}>
+                <li className=' text-2xl text-white font-normal' style={{fontFamily:'Dela Gothic One'}}>home</li>
+              </Link>
+              <Link to={'/seller'}>
+                <li className=' text-2xl text-white font-normal' style={{fontFamily:'Dela Gothic One'}}>Sell your item</li>
+              </Link>
+              <Link to={"/auction"}>
+                <li className=' text-2xl text-white font-normal' style={{fontFamily:'Dela Gothic One'}}>Auction</li>
+              </Link>
             </ul>
             <button style={{fontFamily:'Dela Gothic One'}} type="button" className="focus:outline-none text-black font-extrabold bg-white hover:bg-gray-400 focus:ring-4 focus:ring-purple-300  rounded-2xl text-md px-5 py-2.5 mb-2">Connect Wallet</button>
           </div>
